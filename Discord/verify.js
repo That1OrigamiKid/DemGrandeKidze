2b6afe6ba0f3e693b8a7c1c4ddb144a585cb0c86
That1OrigamiKid <132720108+That1OrigamiKid@users.noreply.github.com>
2023-07-02 12:54:27 -0500

    Update verify.js

--- Discord/verify.js
+++ Discord/verify.js
@@ -2,6 +2,6 @@ inviterequest()
 const names = [canon:"canon", cael:"cael", sydney:"sydney", brigham:"brigham", jaden:"jaden", olivia:"olivia", addy:"addy"]
 function inviterequest() {
   var name = prompt("Please Verify Your Name")
-  if (name == names[name]) {
+  /*if (name == names[name]) {
   window.location.href = "https://discord.gg/fUsjSGb6w"
-  }
+  }*/
