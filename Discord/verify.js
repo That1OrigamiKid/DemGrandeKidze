6c934b8efe57166efd8e20bb798e6c14fb2ccd92
That1OrigamiKid <132720108+That1OrigamiKid@users.noreply.github.com>
2023-07-01 19:02:44 -0500

    Create verify.js

---
+++ Discord/verify.js
@@ -0,0 +1,6 @@
+const names = [Canon, Cael, Sydney, Brigham, Jaden, Olivia, Addy]
+function inviterequest() {
+  var name = prompt("Please Verify Your Name")
+  if ( ["Canon", "Cael", "Sydney", "Brigham", "Jaden", "Olivia", "Addy"].indexOf(name) > -1 ) {
+  window.location.href = "https://discord.gg/fUsjSGb6w"
+  }
