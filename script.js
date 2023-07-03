3bd4eb4d80f57f4a186d4397b334b463c8c8935b
That1OrigamiKid <132720108+That1OrigamiKid@users.noreply.github.com>
2023-07-02 23:15:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function inviterequest() {
   let name = prompt("Whats your name")
-  if (name = "Canon" || "Cael" || "Sydney" || "Brigham" || "Jaden" || "Olivia" || "Addy" || "Charlotte" || "Reed") {
+  if (name = "Canon" || name = "Cael" || name = "Sydney" || name = "Brigham" || name = "Jaden" || name = "Olivia" || name = "Addy" || name = "Charlotte" || name = "Reed") {
     window.location.href = "https://discord.gg/fUsjSGb6w"
   }
 }
