feb7a827c624a9d38bb342ae096fbc3af1500f3b
That1OrigamiKid <132720108+That1OrigamiKid@users.noreply.github.com>
2023-07-02 22:46:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const names = [canon:"canon', cael:"cael", sydney:"sydney", brigham:"brigham", jaden:"jaden", olivia:"olivia", addy:"addy", charlotte:"charlotte", reed:"reed"]
 function inviterequest() {
   let name = prompt("Whats your name")
-  if name = names.name {
+  if (name = names.name) {
   window.location.href = "https://discord.gg/fUsjSGb6w"
   }
 }
