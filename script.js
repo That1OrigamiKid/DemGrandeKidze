c091eaa8404d75d8cc007ddf26ff750333dd018b
That1OrigamiKid <132720108+That1OrigamiKid@users.noreply.github.com>
2023-07-02 21:34:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 names = [canon:"canon', cael:"cael", sydney:"sydney", brigham:"brigham", jaden:"jaden", olivia:"olivia", addy:"addy", charlotte:"charlotte", reed:"reed"]
+function inviterequest() {
+  alert("this button works")
+}
