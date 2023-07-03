0bc5231e69039138411fcfb0c30ef518cb3658e5
That1OrigamiKid <132720108+That1OrigamiKid@users.noreply.github.com>
2023-07-02 23:10:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const names = [canon:"canon', cael:"cael", sydney:"sydney", brigham:"brigham", jaden:"jaden", olivia:"olivia", addy:"addy", charlotte:"charlotte", reed:"reed"]
 function inviterequest() {
   let name = prompt("Whats your name")
   if (name = "Canon" || "Cael" || "Sydney" || "Brigham" || "Jaden" || "Olivia" || "Addy" || "Charlotte" || "Reed") {
