f7d3b1c5ad53db6d8eb44890c726b4383b713fc9
That1OrigamiKid <132720108+That1OrigamiKid@users.noreply.github.com>
2023-07-02 22:26:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-names = [canon:"canon', cael:"cael", sydney:"sydney", brigham:"brigham", jaden:"jaden", olivia:"olivia", addy:"addy", charlotte:"charlotte", reed:"reed"]
+alert("javascript is working")
+//names = [canon:"canon', cael:"cael", sydney:"sydney", brigham:"brigham", jaden:"jaden", olivia:"olivia", addy:"addy", charlotte:"charlotte", reed:"reed"]
 function inviterequest() {
   alert("this button works")
 }
