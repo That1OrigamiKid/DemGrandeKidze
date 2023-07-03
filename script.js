cf4ba601033e58e6a1c92f3b3ae2e57edbed5b90
That1OrigamiKid <132720108+That1OrigamiKid@users.noreply.github.com>
2023-07-03 16:52:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const names = 
+const names = ["Canon", "Cael", "Sydney", "Brigham", "Jaden", "Gwen", "Olivia", "Addy", "Charlotte", "Reed"]
 function inviterequest() {
   let name = prompt("Whats your name")
-  if (name == "Canon" || name == "Cael" || name == "Sydney" || name == "Brigham" || name == "Jaden" || name == "Gwen" || name == "Olivia" || name == "Addy" || name == "Charlotte" || name == "Reed") {
+  //if (name == "Canon" || name == "Cael" || name == "Sydney" || name == "Brigham" || name == "Jaden" || name == "Gwen" || name == "Olivia" || name == "Addy" || name == "Charlotte" || name == "Reed") {
+  if (names.includes(name))
     window.location.href = "https://discord.gg/fUsjSGb6w"
   }
 }
