885196163c77c3b167801d62e7fabd5238932a8f
That1OrigamiKid <132720108+That1OrigamiKid@users.noreply.github.com>
2023-07-16 21:00:17 -0500

    discord invites open in new tabs now (acording to clyde [the discord ai chatbot])

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const names = ["Canon", "Cael", "Sydney", "Brigham", "Jaden", "Gwen", "Olivia", "Addy", "Charlotte", "Reed"]
 function inviterequest() {
   let name = prompt("Whats your name")
-  //if (name == "Canon" || name == "Cael" || name == "Sydney" || name == "Brigham" || name == "Jaden" || name == "Gwen" || name == "Olivia" || name == "Addy" || name == "Charlotte" || name == "Reed") {
   if (names.includes(name)) {
-    window.location.href = "https://discord.gg/fUsjSGb6w"
+    window.open("https://discord.gg/fUsjSGb6w", "_blank");
+    document.getElementById(name) = name
   }
 }
 function tempvcaccess() {
   var ok = prompt('Type "OK" to be redirected')
   if (ok == "OK") {
-    window.location.href = "https://discord.gg/M2xy7XGBM"
+    window.open("discord.gg/M2xy7XGBM", "_blank");
   }
 }
